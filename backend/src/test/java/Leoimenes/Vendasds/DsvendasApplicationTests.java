d6d22757beea113966a21f0ade8454d695cd0fcd
Leo Imenes <leonardoimenes@hotmail.com>
2021-05-04 13:58:09 -0300

    Project created

---
+++ backend/src/test/java/Leoimenes/Vendasds/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package Leoimenes.Vendasds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
