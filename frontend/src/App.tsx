4b8932f1f64d35b7799c06209ea29c60bf115bb8
Leo Imenes <leonardoimenes@hotmail.com>
2021-05-04 15:56:54 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 
 
 function App() {
   return (
-    <div>
+   <> 
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Ola mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+  </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/LeoImenes" target="_blank" rel="noreferrer">Leonardo Imenes</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
